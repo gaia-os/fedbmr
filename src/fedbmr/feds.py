369093d5fba759b817849951190b740152dddadf
dimarkov <5038100+dimarkov@users.noreply.github.com>
2023-01-23 19:07:50 +0100

    added local model inference

--- src/fedbmr/feds.py
+++ src/fedbmr/feds.py
@@ -1,4 +1,8 @@
-from typing import Callable
+from typing import Callable, Dict
+from optax import adabelief
+from numpyro.infer import SVI, TraceGraph_ELBO
+from numpyro.optim import optax_to_numpyro
+from functools import partial
 
 class GenerativeModel(object):
 
@@ -13,7 +17,7 @@ class GenerativeModel(object):
 
     def __call__(self):
         global_params = self.global_prior()
-        local_params = self.local_prior(global_params)
+        local_params = self.local_prior(params=global_params)
         self.likelihood(local_params)
 
 
@@ -41,5 +45,22 @@ class GossipFed(InfFed):
 class BMRFed(InfFed):
     '''Bayesian model reduction for federeated variational inference.
     '''
-    pass
+    global_params: Dict=None
+
+    def local_model(self):
+        local_params = self.local_prior(params=self.global_params)
+        self.likelihood(local_params)
+    
+    def local_guide(self, guide):
+        guide(params=self.global_params)
+
+    def inference(self, rng_key, guide, num_steps=1, *, progress_bar=True):
+        optim = optax_to_numpyro(adabelief(learning_rate=1e-3))
+        self.model = self.local_model
+        self.guide = partial(self.local_guide, guide)
+        svi = SVI(self.model, self.guide, optim, TraceGraph_ELBO(num_particles=10))
+        result = svi.run(rng_key, num_steps=num_steps, progress_bar=progress_bar)
+
+        return result
+
     
